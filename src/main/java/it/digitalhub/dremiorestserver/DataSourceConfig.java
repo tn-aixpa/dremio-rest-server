a170aaa93206658a67916ecc1e2fb2e406c59f10
etomaselli <etomaselli199@gmail.com>
2023-10-13 16:07:01 +0200

    Refined LIST endpoint, added config properties

--- src/main/java/it/digitalhub/dremiorestserver/DataSourceConfig.java
+++ src/main/java/it/digitalhub/dremiorestserver/DataSourceConfig.java
@@ -2,17 +2,39 @@ package it.digitalhub.dremiorestserver;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@ConfigurationProperties(prefix = "flight", ignoreUnknownFields = true)
 public class DataSourceConfig {
+
+    private String driver;
+    private String url;
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     @Bean
     public DataSource mysqlDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.apache.arrow.driver.jdbc.ArrowFlightJdbcDriver");
-        dataSource.setUrl("jdbc:arrow-flight-sql://localhost:32010/?useEncryption=false&disableCertificateVerification=true&user=dremio&password=dremio123");
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
         return dataSource;
     }
 }
--- src/main/java/it/digitalhub/dremiorestserver/DremioController.java
+++ src/main/java/it/digitalhub/dremiorestserver/DremioController.java
@@ -1,5 +1,6 @@
 package it.digitalhub.dremiorestserver;
 
+//import jakarta.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -7,6 +8,7 @@ import static java.util.Map.entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.validation.annotation.Validated;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Validated
 public class DremioController {
 
     //TODO add +, AND, OR, NOT
@@ -36,25 +39,32 @@ public class DremioController {
 	JdbcTemplate jdbcTemplate;
 
     @GetMapping("/{table}")
-	public Page<DremioRecord> list(@PathVariable String table, @RequestParam(required = false) String filter, Pageable pageable) {
-        System.out.println(pageable.getSort());
+	public Page<DremioRecord> list(
+            @PathVariable /*@Pattern(regexp = "\".*\"|[a-zA-Z0-9]+")*/ String table,
+            @RequestParam(required = false) /*@Pattern(regexp = "[a-zA-Z0-9]+")*/ String filter,
+            @RequestParam(required = false) /*@Pattern(regexp = "[a-zA-Z0-9]+")*/ String select,
+            Pageable pageable
+        ) {
         int offset = (int) pageable.getOffset();
         int limit = pageable.getPageSize();
 
-        String query = "SELECT * FROM %s";
+        String query = "SELECT ";
+
+        query += select == null ? "*" : select;
+
+        query += " FROM " + table;
 
         String where = addWhere(filter);
         query += where;
 
         query += addOrderBy(pageable.getSort());
 
-        query += " LIMIT %s OFFSET %s";
+        query += " LIMIT " + limit + " OFFSET " + offset;
 
-        List<Map<String,Object>> map = jdbcTemplate.queryForList(String.format(query, table, limit, offset));
+        List<Map<String,Object>> map = jdbcTemplate.queryForList(query);
         long count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM " + table + where, Long.class);
         List<DremioRecord> result = new ArrayList<>();
         map.forEach(r -> result.add(new DremioRecord(r)));
-        System.out.println("Result: " + result);
         return new PageImpl<>(result, pageable, count);
 	}
 
@@ -65,8 +75,11 @@ public class DremioController {
 
         String where = " WHERE ";
 
-        //filter=id:eq.3,age:eq.30
-        String[] clauses = filter.split(",");
+        //duplicate % character if any
+        filter = filter.replace("%", "%%");
+
+        //example: filter=id:eq.3;age:eq.30
+        String[] clauses = filter.split(";");
         for (String clause : clauses) {
             String[] elements = clause.split(":", 2);
             String[] operatorAndValue = elements[1].split("\\.", 2);
@@ -95,7 +108,8 @@ public class DremioController {
 
 /*
  * TODO
+ * - aggiungere regex ai request params
  * - usare metodi di JdbcTemplate con parametri (es. sostituire queryForList con quello che prende arguments) per prevenire SQL injection (vedere https://github.com/scc-digitalhub/AAC/blob/f00f80d68dc066b93347f06e482a18e6b1d455c0/src/main/java/it/smartcommunitylab/aac/audit/store/AutoJdbcAuditEventStore.java#L36)
- * - creare gli endpoint per LIST e GET (capire come gestire get, es. recuperando la primary key da uno schema)
+ * - creare endpoint per GET (capire come gestire get, es. recuperando la primary key da uno schema)
  * - autenticazione: imitare approccio di PostgREST
  */
